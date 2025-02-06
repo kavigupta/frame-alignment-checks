1360b2358ed3b0c27a41ff4f1357a5720b98d898
Kavi Gupta <kavi@berkeley.edu>
2025-02-06 14:54:21 -0500

    load data

--- src/frame_alignment_checks/data/load.py
+++ src/frame_alignment_checks/data/load.py
@@ -4,54 +4,60 @@ from typing import Tuple
 
 import numpy as np
 import pandas as pd
-import pkg_resources
+
+import frame_alignment_checks
+from importlib.resources import files, as_file
+
 
 from ..coding_exon import CodingExon
 
 
 def load_validation_gene(idx) -> Tuple[np.ndarray, np.ndarray]:
-    path = pkg_resources.resource_filename(
-        "frame_alignment_checks", "data/relevant_validation_genes.npz"
-    )
-    with np.load(path) as data:
-        return data[f"x{idx}"], data[f"y{idx}"]
+    with as_file(
+        files(frame_alignment_checks.data).joinpath("relevant_validation_genes.npz")
+    ) as path:
+        with np.load(path) as data:
+            return data[f"x{idx}"], data[f"y{idx}"]
 
 
 def load_long_canonical_internal_coding_exons():
-    path = pkg_resources.resource_filename(
-        "frame_alignment_checks", "data/long_canonical_internal_coding_exons.pkl"
+    source = files(frame_alignment_checks.data).joinpath(
+        "long_canonical_internal_coding_exons.pkl"
     )
-    with open(path, "rb") as f:
-        return [CodingExon(**d) for d in pickle.load(f)]
+    # with open(path, "rb") as f:
+    with as_file(source) as path:
+        with open(path, "rb") as f:
+            return [CodingExon(**d) for d in pickle.load(f)]
 
 
 def load_minigene(gene, exon):
-    path = pkg_resources.resource_filename(
-        "frame_alignment_checks", f"data/minigene_{gene}_{exon}.pkl"
-    )
-    with open(path, "rb") as f:
-        return pickle.load(f)
+    with as_file(
+        files(frame_alignment_checks.data).joinpath(f"minigene_{gene}_{exon}.pkl")
+    ) as path:
+        with open(path, "rb") as f:
+            return pickle.load(f)
 
 
 def load_saturation_mutagenesis_table():
-    path = pkg_resources.resource_filename(
-        "frame_alignment_checks",
-        "data/saturation_mutagenesis_Supplemental_Table_S2.xlsx",
-    )
-    return pd.read_excel(path)
+    with as_file(
+        files(frame_alignment_checks.data).joinpath(
+            "saturation_mutagenesis_Supplemental_Table_S2.xlsx"
+        )
+    ) as path:
+        return pd.read_excel(path)
 
 
 def load_train_counts_by_phase() -> np.ndarray:
-    path = pkg_resources.resource_filename(
-        "frame_alignment_checks", "data/train_handedness_counts.npz"
-    )
-    with np.load(path) as data:
-        return data["arr_0"]
+    with as_file(
+        files(frame_alignment_checks.data).joinpath("train_handedness_counts.npz")
+    ) as path:
+        with np.load(path) as data:
+            return data["arr_0"]
 
 
 def load_non_stop_donor_windows():
-    path = pkg_resources.resource_filename(
-        "frame_alignment_checks", "data/phase_handedness_test_set.pkl.gz"
-    )
-    with gzip.open(path, "rb") as f:
-        return pickle.load(f)
+    with as_file(
+        files(frame_alignment_checks.data).joinpath("phase_handedness_test_set.pkl.gz")
+    ) as path:
+        with gzip.open(path, "rb") as f:
+            return pickle.load(f)
