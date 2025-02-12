6de47632e672ca52753d8d7f2d62cfb3216f413e
Kavi Gupta <kavi@berkeley.edu>
2025-02-12 00:48:59 -0500

    fix lint

--- tests/replace_3mer_test.py
+++ tests/replace_3mer_test.py
@@ -2,7 +2,6 @@ import unittest
 
 import numpy as np
 
-import frame_alignment_checks as fac
 from frame_alignment_checks.replace_3mer.stop_codon_replacement import (
     stop_codon_replacement_delta_accuracy,
 )
